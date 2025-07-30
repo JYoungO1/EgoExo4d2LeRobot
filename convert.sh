989b642e53b1ba90a2646ce714180fa3cca41163
Jaeyoung Choi <125002019+JYoungO1@users.noreply.github.com>
2025-07-30 14:24:36 +0900

    Create convert.sh

---
+++ convert.sh
@@ -0,0 +1,10 @@
+export SVT_LOG=1
+export HF_DATASETS_DISABLE_PROGRESS_BARS=TRUE
+export HDF5_USE_FILE_LOCKING=FALSE
+
+python libero_h5.py \
+    --json-dir /path/to/EgoExo4d_hand_annotations/ \
+    --video-dir /path/to/EgoExo_video \
+    --takes-path /path/to/takes.json \
+    --output-dir /path/to/local \
+    --aggregate-name egoexo_lerobot
