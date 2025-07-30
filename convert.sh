bf8431de29c4c4601974df04a83f00594c9de545
Jaeyoung Choi <125002019+JYoungO1@users.noreply.github.com>
2025-07-30 14:33:28 +0900

    Update convert.sh

--- convert.sh
+++ convert.sh
@@ -2,7 +2,7 @@ export SVT_LOG=1
 export HF_DATASETS_DISABLE_PROGRESS_BARS=TRUE
 export HDF5_USE_FILE_LOCKING=FALSE
 
-python libero_h5.py \
+python egoexo4d2LeRobot.py \
     --json-dir /path/to/EgoExo4d_hand_annotations/ \
     --video-dir /path/to/EgoExo_video \
     --takes-path /path/to/takes.json \
